8e0784f17c4735ce858473726aa0d5ed60ec75c7
Ana Paula Lopes Araujo <anapaulalopesaraujo@MacBook-Pro-de-Ana.local>
2020-01-19 10:37:32 -0300

    Word Blanks

---
+++ basic-javascript/word-blanks.js
@@ -0,0 +1,6 @@
+var myNoun = "dog";
+var myAdjective = "big";
+var myVerb = "ran";
+var myAdverb = "quickly";
+
+var wordBlanks = "My " + myNoun + " was in a " + myAdjective + " house and " + myVerb + " in the living room very " + myAdverb + "."; // Only change this line;
